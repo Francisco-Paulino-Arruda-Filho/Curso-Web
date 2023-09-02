60d8ba4f2f6baaa04077074b0b7dd3b76fe77e18
Paulino Filho <fpaulinofilho04@gmail.com>
2023-09-01 22:33:08 -0300

    Commit init

---
+++ javascript/js/concatenacao.js
@@ -0,0 +1,3 @@
+var nome = prompt('Digite seu nome:')
+var numero = prompt('Digite sua idade:')
+document.write('<h1>Olá ' + nome + ', tudo nem?' + ' Estou vendo que voce tem ' + numero + ' anos</h1>')
---
+++ javascript/js/index.js
@@ -0,0 +1,14 @@
+//Muito usado para debug, abre um dialog
+alert('Vai se foder João Pedro!')
+/*Pega o elemento de ID igual ao argumento passado e atualiza com uma string*/
+document.getElementById('nome').value = 'Oi';
+//String
+var text = "Morte ao Java"
+//Número fracionário
+var numberoFracionario = 1100.75
+//Número inteiro
+var numeroInteiro = -7
+//Boolean
+var teste = true
+document.write(text)
+console.log(numeroInteiro)
